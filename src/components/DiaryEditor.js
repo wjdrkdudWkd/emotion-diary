691feb5fa1cf5b82e76b0cc71f54043cf28d5af6
wjdrkdudwkd <wjdrkdudwkd@playnomm.com>
2023-08-09 20:09:49 +0900

    /new

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,8 +1,9 @@
 import MyHeader from './MyHeader';
 import MyButton from './MyButton';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useContext, useRef, useState } from 'react';
 import EmotionItem from './EmotionItem';
+import { DiaryDispatchContext } from "./../App.js"
 
 const emotionList = [
   {
@@ -11,24 +12,24 @@ const emotionList = [
     emotion_descript: '완전 좋음',
   },
   {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion1.png',
-    emotion_descript: '완전 좋음',
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + '/assets/emotion2.png',
+    emotion_descript: '좋음',
   },
   {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion1.png',
-    emotion_descript: '완전 좋음',
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + '/assets/emotion3.png',
+    emotion_descript: '보통',
   },
   {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion1.png',
-    emotion_descript: '완전 좋음',
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + '/assets/emotion4.png',
+    emotion_descript: '별로',
   },
   {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + '/assets/emotion1.png',
-    emotion_descript: '완전 좋음',
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + '/assets/emotion5.png',
+    emotion_descript: '완전 별로',
   },
 ];
 
@@ -37,9 +38,28 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
-  const [emotion, setEmotion] = useState(3);
-  const [date, setDate] = useState(getStringDate(new Date()));
-  const navigate = useNavigate();
+const contentRef = useRef();
+const [content, setContent] = useState("");
+const [emotion, setEmotion] = useState(3);
+const [date, setDate] = useState(getStringDate(new Date()));
+const { onCreate } = useContext(DiaryDispatchContext);
+
+const handleClickEmote = (emotion) => {
+  setEmotion(emotion);
+}
+
+const handleSubmit = () => {
+  if(content.length < 1) {
+    contentRef.current.focus();
+    return ;
+  }
+  
+  onCreate(date, content, emotion);
+  navigate('/', {replace : true});
+}
+
+const navigate = useNavigate();
+
 
   return (
     <div className="DiaryEditor">
@@ -61,10 +81,34 @@ const DiaryEditor = () => {
         </section>
         <section>
           <h4>emotion</h4>
-          <div className="input_box_emotion_list_wrapper">
+          <div className="input_box emotion_list_wrapper">
             {emotionList.map((it) => (
-              <EmotionItem key={it.emotion_id} {...it} />
+              <EmotionItem key={it.emotion_id} {...it} 
+              onClick={handleClickEmote} 
+              isSelected={it.emotion_id === emotion}
+              />
             ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className='input_box text_wrapper'>
+              <textarea ref={contentRef} 
+              value={content} 
+              onChange={(e) => setContent(e.target.value)} 
+              placeholder='오늘 하루는 어땠나요?'
+              />
+          </div>
+        </section>
+        <section>
+          <div className='control_box'>
+            <MyButton 
+              text={"cancel"} 
+              onClick={() => navigate(-1)} />
+            <MyButton 
+              text={"save"} 
+              type={"positive"} 
+              onClick={handleSubmit}/>
           </div>
         </section>
       </div>
--- src/components/EmotionItem.js
+++ src/components/EmotionItem.js
@@ -1,8 +1,9 @@
-const EmotionItem = ({ emotion_id, emotion_img, emotion_discript }) => {
+const EmotionItem = ({ emotion_id, emotion_img, emotion_descript, onClick, isSelected}) => {
   return (
-    <div className="EmotionItem">
+    <div onClick={() => onClick(emotion_id)} className={["EmotionItem", 
+    isSelected ? `EmotionItem_on_${emotion_id}`: `EmotionItem_off`,].join(" ")}>
       <img src={emotion_img} />
-      <span>{emotion_discript}</span>
+      <span>{emotion_descript}</span>
     </div>
   );
 };
